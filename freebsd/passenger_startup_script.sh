e289f2e65fe19c84bbb0215a938599ff7397a28a
Allen Garvey <allen.garvey@gmail.com>
2017-01-03 20:26:18 -0500

    Edit passenger startup script for directory script should be placed in

--- freebsd/passenger_startup_script.sh
+++ freebsd/passenger_startup_script.sh
@@ -1,6 +1,6 @@
 # example of startup script for Phusion Passenger standalone
 # start passenger server once manually as 'sudo' first so passenger is installed
-# place file in /etc/rc.d
+# place file in /usr/local/etc/rc.d
 # chmod +x to make script executable
 # set rcvar to "YES" in /etc/rc.conf
 # start and stop manually with `sudo service passenger_booklist onestart` or `sudo service passenger_booklist onestop`
