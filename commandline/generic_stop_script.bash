4a6e439b3d6fc821213dd7dc3996e0a68f479d94
Allen Garvey <allen.garvey@gmail.com>
2017-03-31 17:52:11 -0400

    Add note about generic bash stop script

---
+++ commandline/generic_stop_script.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#example script to stop application running in background, such as a 
+#service running on FreeBSD
+
+app_name="example.jar"
+pid=`ps aux | grep $app_name | sort | awk 'NR==1{print $2}'`
+kill -9 $pid > /dev/null 2>&1
