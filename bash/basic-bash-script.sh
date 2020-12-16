21e52f0d36c59ed5d2cdcc5f0cc8a8dac8a1c798
Allen Garvey <allengarvey@hotmail.com>
2020-12-15 21:46:21 -0500

    Add documentation for basic bash script

---
+++ bash/basic-bash-script.sh
@@ -0,0 +1,7 @@
+# Minimal prelude to bash script, based on 
+# https://betterdev.blog/minimal-safe-bash-script-template/
+# for explanation of set flags, see https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+
+#!/usr/bin/env bash
+
+set -Eeuo pipefail;
