479ace1800c20c6ee030d9be2e41b45803c9b7bb
Allen Garvey <allengarvey@hotmail.com>
2023-09-23 12:16:30 -0400

    Add script to batch convert images to webp

---
+++ bash/batch-convert-to-webp.sh
@@ -0,0 +1,1 @@
+mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -quality 90 -set filename:name '%t' 'converted/%[filename:name].webp'
