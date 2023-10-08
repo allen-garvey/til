497f426ad4e2b3a49c8a070e5e871ef220ba4e99
Allen Garvey <allengarvey@hotmail.com>
2023-10-08 12:11:21 -0400

    Set extract frames fps to 6

--- video/video-to-and-from-frames.sh
+++ video/video-to-and-from-frames.sh
@@ -1,8 +1,8 @@
 # Note that %04 is number of digits in frame numbers
 # so if extracting a higher amount of frames you will need a higher number
 
-# Extract video to frames at 8 fps
-mkdir -p output-directory && ffmpeg -i input.mp4 -vf fps=8 './output-directory/%04d.jpg'
+# Extract video to frames at 6 fps
+mkdir -p output-directory && ffmpeg -i input.mp4 -vf fps=6 './output-directory/%04d.jpg'
 
-# convert frames back to video at 8 fps
-ffmpeg -framerate 8 -f image2 -i ./%4d.png out.mp4
+# convert frames back to video at 6 fps
+ffmpeg -framerate 6 -f image2 -i ./%4d.png out.mp4
