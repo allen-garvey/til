b395923ef5253346622a8cd7fe7e26b7ada1f4cb
Allen Garvey <allengarvey@hotmail.com>
2023-10-07 23:01:32 -0400

    Document how to extract frames to a different directory

--- video/video-to-and-from-frames.sh
+++ video/video-to-and-from-frames.sh
@@ -2,7 +2,7 @@
 # so if extracting a higher amount of frames you will need a higher number
 
 # Extract video to frames at 8 fps
-ffmpeg -i input.mp4 -vf fps=8 '%04d.jpg'
+mkdir -p output-directory && ffmpeg -i input.mp4 -vf fps=8 './output-directory/%04d.jpg'
 
 # convert frames back to video at 8 fps
 ffmpeg -framerate 8 -f image2 -i ./%4d.png out.mp4
