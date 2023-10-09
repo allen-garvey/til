9aa758344753c8df9b70204ddab736aa9ece7438
Allen Garvey <allengarvey@hotmail.com>
2023-10-08 21:42:02 -0400

    Add to video script to fix video dimensions

--- video/video-to-and-from-frames.sh
+++ video/video-to-and-from-frames.sh
@@ -8,4 +8,4 @@ mkdir -p output-directory && ffmpeg -i input.mp4 -vf fps=6 './output-directory/%
 ffmpeg -framerate 6 -f image2 -i ./%4d.png out.mp4
 
 # to re-encode at 24 fps
-ffmpeg -i <input> -filter:v fps=24 <output>
+ffmpeg -i <input> -filter:v fps=24 -pix_fmt yuv420p -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" <output>
