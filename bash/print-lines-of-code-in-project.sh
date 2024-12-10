5c7b74920da1572a78d9153979b5877bae6a0874
Allen Garvey <allen.garvey@gmail.com>
2024-12-09 23:21:49 -0500

    Add bash script to print lines of code in a project

---
+++ bash/print-lines-of-code-in-project.sh
@@ -0,0 +1,5 @@
+# web project
+find . | egrep '^\./(node_modules|.git|public_html)/' --invert-match | egrep '\.(js|php|vue)$' | xargs wc -l | sort -nr | less
+
+# elixir phoenix project
+find . | egrep '^\./(node_modules|.git|public_html|deps)/|priv/.+/assets/.*$|_test\.exs$|test/support/.*\.ex' --invert-match | egrep '\.(js|php|vue|ex|eex|exs)$' | xargs wc -l | sort -nr | less
