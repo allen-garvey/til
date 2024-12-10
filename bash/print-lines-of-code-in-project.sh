306da73ce1ad2a40d39dfae93c896a82fc9043b5
Allen Garvey <allen.garvey@gmail.com>
2024-12-09 23:31:48 -0500

    Update lines of code script to support typescript as well as jsx

--- bash/print-lines-of-code-in-project.sh
+++ bash/print-lines-of-code-in-project.sh
@@ -1,5 +1,5 @@
 # web project
-find . | egrep '^\./(node_modules|.git|public_html)/' --invert-match | egrep '\.(js|php|vue)$' | xargs wc -l | sort -nr | less
+find . | egrep '^\./(node_modules|.git|public_html)/' --invert-match | egrep '\.(js|ts|jsx|tsx|php|vue)$' | xargs wc -l | sort -nr | less
 
 # elixir phoenix project
 find . | egrep '^\./(node_modules|.git|public_html|deps)/|priv/.+/assets/.*$|_test\.exs$|test/support/.*\.ex' --invert-match | egrep '\.(js|php|vue|ex|eex|exs)$' | xargs wc -l | sort -nr | less
