a177c430d6f06e4bb5a1b9580c51caff6a947d1c
Allen Garvey <allen.garvey@gmail.com>
2024-12-16 15:42:52 -0500

    Add pixel format to script to convert frames to video

--- video/video-to-and-from-frames.sh
+++ video/video-to-and-from-frames.sh
@@ -5,7 +5,7 @@
 mkdir -p output-directory && ffmpeg -i input.mp4 -vf fps=6 './output-directory/%04d.jpg'
 
 # convert frames back to video at 6 fps
-ffmpeg -framerate 6 -f image2 -i ./%4d.png out.mp4
+ffmpeg -framerate 6 -f image2 -i ./%4d.webp -filter:v format=pix_fmts='yuv420p' out.mp4
 
 # to re-encode at 24 fps
 ffmpeg -i <input> -filter:v fps=24 -pix_fmt yuv420p -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" <output>
