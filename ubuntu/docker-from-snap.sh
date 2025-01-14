7c3be3e5545b71d73257b6db56e03052b3729b00
Allen Garvey <allen.garvey@gmail.com>
2025-01-14 12:23:38 -0500

    Update steps for docker snap installation

--- ubuntu/docker-from-snap.sh
+++ ubuntu/docker-from-snap.sh
@@ -4,7 +4,7 @@ sudo snap install docker
 # create docker group and assign current user to it so we don't need sudo
 sudo groupadd docker
 sudo gpasswd -a $USER docker
-newgrp docker
+# restart computer
 # to find service name
 sudo systemctl list-units --type=service --all
 sudo systemctl start snap.docker.dockerd.service
